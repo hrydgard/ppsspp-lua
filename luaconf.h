f3271af11ab8591164b871e36520a7210964f3f6
Henrik Rydgård <hrydgard@gmail.com>
2024-10-24 01:37:22 +0200

    UWP patch: pretend not to be Windows

--- luaconf.h
+++ luaconf.h
@@ -11,6 +11,9 @@
 #include <limits.h>
 #include <stddef.h>
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
 
 /*
 ** ===================================================================
@@ -43,14 +46,18 @@
 */
 /* #define LUA_USE_C89 */
 
+#if defined(WINAPI_FAMILY) && defined(WINAPI_FAMILY_PARTITION)
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP) && WINAPI_FAMILY != WINAPI_FAMILY_DESKTOP_APP
+#define LUA_USE_UWP 1
+#endif
+#endif
 
 /*
 ** By default, Lua on Windows use (some) specific Windows features
 */
-#if !defined(LUA_USE_C89) && defined(_WIN32) && !defined(_WIN32_WCE)
+#if !defined(LUA_USE_C89) && defined(_WIN32) && !defined(_WIN32_WCE) && !defined(LUA_USE_UWP)
 #define LUA_USE_WINDOWS  /* enable goodies for regular Windows */
 #endif
-
 
 #if defined(LUA_USE_WINDOWS)
 #define LUA_DL_DLL	/* enable support for DLL */
